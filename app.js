d277a0c57e7762fc18440ea0a0cf668d499b4ba7
nmaryy <maryamnaderi007@gmail.com>
2023-08-17 15:48:45 +0330

    Update app.js

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ function showWeather(response) {
   wind[0].innerHTML = `${cityWind}km/h`;
   wind[1].innerHTML = `${cityWind}km/h`;
   desc.innerHTML = cityDesc;
-  sunset.innerHTML = sunsetresponse.toUTCString();
+  sunset.innerHTML = sunsetresponse.toGMTString();
   // precip.innerHTML = precipitation;
   feels.innerHTML = `${feelsLike}°C`;
   visib.innerHTML = `${visibility / 1000}km`;
